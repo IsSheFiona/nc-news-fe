a14c4d4231ae6bef99925b51ad17f11f0dc32c70
Fiona Fairbairn <fairbairnf@gmail.com>
2019-07-15 11:06:52 +0100

    react project set up and rendering

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+//import axios from "axios";
+import "./App.css";
+import { Router, Link } from "@reach/router";
+
+class App extends React.Component {
+  state = {};
+
+  render() {
+    return (
+      <h1>let's make nc news work...</h1>
+      // <React.Fragment>
+      //   <nav>
+      //     <Link to="/">
+      //       <h3>Home</h3>
+      //     </Link>
+      //     <Link to="/coding">
+      //       <h3>Coding</h3>
+      //     </Link>
+      //     <Link to="/football">
+      //       <h3>Football</h3>
+      //     </Link>
+      //     <Link to="/cooking">
+      //       <h3>Cooking</h3>
+      //     </Link>
+      //   </nav>
+      //   <Home path="/" />
+      //   <Router />
+      // </React.Fragment>
+    );
+  }
+
+  //componentDidMount() {}
+}
+
+export default App;
