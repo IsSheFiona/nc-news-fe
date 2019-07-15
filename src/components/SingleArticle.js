3d536461f1d51a5d4e5c5a6aff249a7e9ca9caab
Fiona Fairbairn <fairbairnf@gmail.com>
2019-07-15 16:11:05 +0100

    added comments link

--- src/components/SingleArticle.js
+++ src/components/SingleArticle.js
@@ -1,25 +1,28 @@
 import React from "react";
 import axios from "axios";
+import { Link } from "@reach/router";
 
 class SingleArticle extends React.Component {
   state = {
     singleArticle: {}
   };
   render() {
+    console.log(this.state);
     return (
       <React.Fragment>
         <h3>{this.state.singleArticle.title}</h3>
         <p>{this.state.singleArticle.body}</p>
-        <p>Author~~{this.state.singleArticle.author}</p>
-        <p>Date Added~~{this.state.singleArticle.created_at}</p>
-        <p>Votes~~{this.state.singleArticle.votes}</p>
-        <p>Comments~~{this.state.singleArticle.comment_count}</p>
+        <p>Author{":  " + this.state.singleArticle.author}</p>
+        <p>Date Added{":  " + this.state.singleArticle.created_at}</p>
+        <p>Votes{":  " + this.state.singleArticle.votes}</p>
+        <Link to={`/articles/${this.state.singleArticle.article_id}/comments`}>
+          <p>Comments{":  " + this.state.singleArticle.comment_count}</p>
+        </Link>
       </React.Fragment>
     );
   }
 
   fetchAnArticle = () => {
-    console.log(this.props);
     const url = `https://fionas-nc-news.herokuapp.com/api/articles/${
       this.props.article_id
     }`;
