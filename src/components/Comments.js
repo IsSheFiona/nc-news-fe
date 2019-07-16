dc9526025c99f651a7c84f149efc70ad0139744a
Fiona Fairbairn <fairbairnf@gmail.com>
2019-07-16 17:00:58 +0100

    comment posting function implemented

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,13 +1,19 @@
 import React from "react";
 import axios from "axios";
+import CommentAdder from "./CommentAdder";
 
 class Comments extends React.Component {
   state = {
     comments: []
   };
   render() {
+    console.log(this.props);
     return (
       <React.Fragment>
+        <CommentAdder
+          loggedInUser={this.props.loggedInUser}
+          postAComment={this.postAComment}
+        />
         <ul>
           {this.state.comments.map(comment => {
             return (
@@ -40,6 +46,20 @@ class Comments extends React.Component {
         this.setState({ err });
       });
   };
+
+  postAComment = ({ body, loggedInUser }) => {
+    const url = `https://fionas-nc-news.herokuapp.com/api/articles/${
+      this.props.article_id
+    }/comments`;
+    console.log(loggedInUser);
+    axios.post(url, { body, username: loggedInUser }).then(response => {
+      console.log(response);
+      this.setState(prevState => {
+        return { comments: [response.data.comment, ...prevState.comments] };
+      });
+    });
+  };
+
   componentDidMount() {
     this.fetchComments();
   }
