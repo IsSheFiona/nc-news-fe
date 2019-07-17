5bdee4ad98d805a62b3e46d42d9694799546fe6f
Fiona Fairbairn <fairbairnf@gmail.com>
2019-07-17 16:21:30 +0100

    updated formatting

--- src/components/SingleArticleCard.js
+++ src/components/SingleArticleCard.js
@@ -1,15 +1,22 @@
 import React from "react";
+import Voter from "./Voter";
 
 function SingleArticleCard(props) {
   return (
-    <React.Fragment>
-      <h3>{props.singleArticle.title}</h3>
-      <p>{props.singleArticle.body}</p>
-      <p>Author{":  " + props.singleArticle.author}</p>
-      <p>Date Added{":  " + props.singleArticle.created_at}</p>
-      <p>Votes{":  " + props.singleArticle.votes}</p>
-      <p>Comments{":  " + props.singleArticle.comment_count}</p>
-    </React.Fragment>
+    <>
+      <div className="singleArticle">
+        <h3>{props.singleArticle.title}</h3>
+        <p>{props.singleArticle.body}</p>
+        <p>Author{":  " + props.singleArticle.author}</p>
+        <p>Date Added{":  " + props.singleArticle.created_at}</p>
+        <Voter
+          votes={props.singleArticle.votes}
+          id={props.singleArticle.article_id}
+          type="articles"
+        />
+        <p>Comments{":  " + props.singleArticle.comment_count}</p>
+      </div>
+    </>
   );
 }
 
