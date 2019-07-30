85aedbfd7acc292bb95e23bebadd296d8245323a
Fiona Fairbairn <fairbairnf@gmail.com>
2019-07-30 17:20:52 +0100

    base url extracted

--- src/api.js
+++ src/api.js
@@ -1,34 +1,31 @@
 import axios from "axios";
 
+${ url } /articles
+
+const url = "https://fionas-nc-news.herokuapp.com/api"
+
 export function getArticles(topic, sort_by, order, p = 1) {
-  const url = "https://fionas-nc-news.herokuapp.com/api/articles";
-  return axios.get(url, {
+  return axios.get(`${url}/articles`, {
     params: { topic: topic, sort_by: sort_by, order: order, p: p }
   });
 }
 
 export function getComments(article_id) {
-  const url = `https://fionas-nc-news.herokuapp.com/api/articles/${article_id}/comments`;
-
-  return axios.get(url);
+  return axios.get(`${url}/articles/${article_id}/comments`);
 }
 
 export function addComment(body, loggedInUser, article_id) {
-  const url = `https://fionas-nc-news.herokuapp.com/api/articles/${article_id}/comments`;
-  return axios.post(url, { body, username: loggedInUser });
+  return axios.post(`${url}/articles/${article_id}/comments`, { body, username: loggedInUser });
 }
 
 export function removeComment(comment, comment_id) {
-  const url = `https://fionas-nc-news.herokuapp.com/api/comments/${comment_id}`;
-  return axios.delete(url);
+  return axios.delete(`${url}/comments/${comment_id}`);
 }
 
 export function getAnArticle(article_id) {
-  const url = `https://fionas-nc-news.herokuapp.com/api/articles/${article_id}`;
-  return axios.get(url);
+  return axios.get(`${url}/articles/${article_id}`);
 }
 
 export function vote(value, type, id) {
-  const url = `https://fionas-nc-news.herokuapp.com/api/${type}/${id}`;
-  return axios.patch(url, { inc_votes: value });
+  return axios.patch(`${url}${type}/${id}`, { inc_votes: value });
 }
